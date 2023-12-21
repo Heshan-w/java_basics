99638524f0474f5cfb5a61b723fd4af7ba191609
Heshan-w <htwanigasinghe@gmail.com>
2023-12-21 15:17:34 +0530

    "arraylists" class-file updated, and other non-java-code related files updated

--- arraylists.java
+++ arraylists.java
@@ -7,8 +7,8 @@ public class arraylists {
         // but arrays can't and array need to be either initialized with data or its size when
         // -declaring it first...arraylists on the other hand do not.
 
-        // exapmle
-        ArrayList<String> cars = new ArrayList<>(Arrays.asList("porsche", "audi", "BMW", "ferarri", "toyota" ));
+        // example
+        ArrayList<String> cars = new ArrayList<>(Arrays.asList("Porsche", "Audi", "BMW", "Ferarri", "Toyota" ));
         ArrayList<Integer> oddNumbers = new ArrayList<>(Arrays.asList(1, 3, 5, 7, 9, 11, 13, 15));
 
         System.out.println("size of 'cars' arraylist : " + (cars.size()));
@@ -16,9 +16,10 @@ public class arraylists {
         System.out.println(cars);
         System.out.println(oddNumbers);
         System.out.println("item at index 2 in cars arraylist : " + cars.get(2));
-        cars.add("mercedes");
+        cars.add("Mercedes");
+        cars.add("Honda");
         cars.remove(3);
-        cars.remove("audi");
+        cars.remove("Audi");
         System.out.println(cars);
     }
 }
